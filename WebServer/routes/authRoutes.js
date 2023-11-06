c7feea07f840e33c15838635b45805c92376e653
leechiyeong <130368957+leechiyeong@users.noreply.github.com>
2023-11-06 21:00:15 +0900

    프론트 부분 삭제

--- WebServer/routes/authRoutes.js
+++ WebServer/routes/authRoutes.js
@@ -10,6 +10,5 @@ router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
 router.post('/projects',authMiddleware.authMiddleware ,projectController.createProject);
-router.get('/users/search', projectController.searchUsers);
 
 module.exports = router;
