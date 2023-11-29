7395d60b05b32559b0926fbfc5ff6239be425913
SangJin <sj_6612@kumoh.ac.kr>
2023-11-29 23:47:27 +0900

    work 및 mypage

--- Front/project/src/collaboflow/mypage.js
+++ Front/project/src/collaboflow/mypage.js
@@ -4,38 +4,70 @@ class Mypage extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            currentPassword:"",
+            userEmail:"",
+            userName:"",
+            equalPassword:"",
             changedPassword:"",
             userId: localStorage.getItem('userId'),
         }
     }
+    componentDidMount() {
+      const userId = this.state.userId; // userId 가져오기
+  
+      const url = `http://localhost:3000/myPage?userId=${encodeURIComponent(userId)}`
+  
+        fetch(url)
+        
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('네트워크 응답이 올바르지 않습니다');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          this.setState({
+            userName: data.name,
+            userEmail:data.email,
+          });
+          console.log(data)
+          console.log('email:',this.state)
+        })
+        .catch((error) => {
+          console.error('에러:', error);
+        });
+    }
+    handleEqualPasswordChange = (event) => {
+      this.setState({ equalPassword: event.target.value });
+    };
+
     handlePasswordChange = (event) => {
         this.setState({ changedPassword: event.target.value });
-      };
+    };
     
       handleFormSubmit = (event) => {
         event.preventDefault();
-    
-        // Add your fetch logic here to update the password
-        // using this.state.changedPassword and this.state.userId
-        // Example:
-        fetch('your-api-endpoint', {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            userId: this.state.userId,
-            newPassword: this.state.changedPassword,
-          }),
-        })
-          .then(response => response.json())
-          .then(data => {
-            // Handle success or error
+        const userId = this.state.userId;
+        const equalPassword = this.state.equalPassword;
+        const changedPassword = this.state.changedPassword;
+
+        if(equalPassword==changedPassword){
+  
+          fetch(`http://localhost:3000/myPage?userId=${encodeURIComponent(userId)}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              userId: this.state.userId,
+              pw: this.state.changedPassword,
+            }),
           })
-          .catch(error => {
-            // Handle error
-          });
+            .catch(error => {
+            });
+          }
+          else{
+            alert("입력한 비밀번호가 다릅니다");
+          }
       };
 
     render() {
@@ -59,31 +91,31 @@ class Mypage extends React.Component {
                       {/* Floating Labels Form */}
                         <form>
                         <div className="row mb-3">
-                            <label htmlFor="inputText" className="col-sm-2 col-form-label">Name</label>
+                            <label  className="col-sm-2 col-form-label">Name</label>
                             <div className="col-sm-10">
-                                <input type="text" className="form-control" id="inputText" readOnly />
+                                <div type="text" className="form-control" readOnly>{this.state.userName}</div>
                             </div>
                             </div>
                             <div className="row mb-3">
-                            <label htmlFor="inputEmail" className="col-sm-2 col-form-label">Email</label>
+                            <label className="col-sm-2 col-form-label">Email</label>
                             <div className="col-sm-10">
-                                <input type="email" className="form-control" id="inputEmail" readOnly/>
+                                <div  className="form-control" readOnly>{this.state.userEmail}</div>
                             </div>
                             </div>
                             <div className="row mb-3">
                               {/* Adjusted grid system for password fields */}
-                              <label htmlFor="inputPassword" className="col-sm-3 col-form-label">현재 비밀번호</label>
+                              <label htmlFor="inputPassword" className="col-sm-3 col-form-label">새 비밀번호</label>
                               <div className="col-sm-8">
                               <input
                                     type="password"
                                     className="form-control"
                                     id="inputPassword"
-                                    //onChange={}
+                                    onChange={this.handleEqualPasswordChange}
                                     />
                               </div>
                             </div>
                             <div className="row mb-3">
-                              <label htmlFor="inputPassword" className="col-sm-3 col-form-label">새 비밀번호</label>
+                              <label htmlFor="inputPassword" className="col-sm-3 col-form-label">비밀번호 확인</label>
                               <div className="col-sm-8">
                                     <input
                                     type="password"
@@ -96,7 +128,7 @@ class Mypage extends React.Component {
                             
                             <div className="row justify-content-center">
                                 <div className="col-auto">
-                                    <button type="submit" className="btn btn-primary">수정</button>
+                                    <button type="submit" className="btn btn-primary" onClick={this.handleFormSubmit}>수정</button>
                                 </div>
                                 <div className="col-auto">
                                     <button type="submit" className="btn btn-secondary">취소</button>
@@ -106,9 +138,9 @@ class Mypage extends React.Component {
     
                     </div>
                   </div>
-                  <div className="col-auto">
+                  {/* <div className="col-auto">
                         <button type="submit" className="btn btn-danger">회원 탈퇴</button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
             </section>
