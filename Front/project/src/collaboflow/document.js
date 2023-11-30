0027772f582f1f80a3e1ecceb4f3fd4ef3e08b89
leechiyeong <130368957+leechiyeong@users.noreply.github.com>
2023-11-30 23:20:31 +0900

    yorkie 추가

--- Front/project/src/collaboflow/document.js
+++ Front/project/src/collaboflow/document.js
@@ -1,40 +1,95 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import yorkie from 'yorkie-js-sdk';
 
-class Document extends React.Component{
-  
-  render(){
-    return(<main id="main" className="main">
-    <div className="pagetitle">
-      <h1>문서</h1>
-      
-    </div>
+class Document extends React.Component {
+  constructor(props) {
+    super(props);
+    this.editorRef = React.createRef();
+    this.state = {
+      doc: null,
+      client: null
+    };
+  }
 
-    <section className="section">
-      <div className="row">
+  async componentDidMount() {
+    // Create a Yorkie client and activate it
+    const client = new yorkie.Client('https://api.yorkie.dev', {
+      apiKey: 'cli78fbprhcevnm8qp8g' // Your API Key here
+    });
+    await client.activate();
 
-        <div className="col-xl-10">
+    // Create a document and attach it to the client
+    const doc = new yorkie.Document('editor');
+    await client.attach(doc);
 
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">New Document</h5>
-              <textarea className="tinymce-editor" style={{ width: '100%', height: '300px' }} >
-              </textarea>
-            </div>
-            <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                  >
+    // Subscribe to document changes
+    doc.subscribe((event) => {
+      if (event.type === 'remote-change') {
+        this.editorRef.current.value = doc.getRoot().text;
+      }
+    });
+
+    // Set up the input listener
+    this.editorRef.current.addEventListener('input', () => {
+      doc.update((root) => {
+        root.text = this.editorRef.current.value;
+      });
+    });
+
+    // Initialize the document text
+    doc.update((root) => {
+      if (root.text) {
+        this.editorRef.current.value = root.text;
+      } else {
+        root.text = 'Edit me.';
+      }
+    });
+  }
+
+  async componentWillUnmount() {
+    // Detach the document and deactivate the client
+    if (this.state.doc) {
+      await this.state.client.detach(this.state.doc);
+    }
+    if (this.state.client) {
+      await this.state.client.deactivate();
+    }
+
+    // Reset the state
+    this.setState({ doc: null, client: null });
+  }
+
+  render() {
+    return (
+      <main id="main" className="main">
+        <div className="pagetitle">
+          <h1>문서</h1>
+        </div>
+
+        <section className="section">
+          <div className="row">
+            <div className="col-xl-10">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">New Document</h5>
+                  <textarea
+                    className="tinymce-editor"
+                    ref={this.editorRef}
+                    style={{ width: "100%", height: "300px" }}
+                  ></textarea>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-primary">
                     OK
                   </button>
                 </div>
+              </div>
+            </div>
           </div>
-
-        </div>
-      </div>
-    </section>
-  </main>
-  );};
+        </section>
+      </main>
+    );
+  }
 }
 
 export default Document;
